42ca6a45f509b60fe1cb001d8e0c2f1987273125
tmoriya <moriya.takayasu514@gmail.com>
2022-05-28 15:16:12 +0900

    set title bar

---
+++ src/components/TitleBar.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+
+
+
+export default function TitleBar() {
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        width: '100%',
+        background: 'linear-gradient(45deg, #2196f3 30%, #21cbf3 90%)',
+        boxShadow: '0 3px 5px 2px rgba(33, 203, 243, .30)',
+      }}
+    >
+      <Typography variant="h5" color="inherit"
+        sx={{
+          flex: 1,
+          padding: '0.5em',
+          textAlign: 'left'
+        }}>
+        Multi Word Counter
+      </Typography>
+    </AppBar >
+  );
+}
