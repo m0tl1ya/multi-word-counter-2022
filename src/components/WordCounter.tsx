d5f2433a69280dd71f0681f12930b1147c9642ea
tmoriya <moriya.takayasu514@gmail.com>
2022-06-04 16:31:13 +0900

    文字数カウントの削除処理の追加

--- src/components/WordCounter.tsx
+++ src/components/WordCounter.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { useCallback } from 'react';
+
 import AppBar from '@mui/material/AppBar';
 import Typography from '@mui/material/Typography';
 
@@ -6,21 +8,36 @@ import { Paper } from '@mui/material';
 import { IconButton, Switch } from '@mui/material';
 import { Clear, CountertopsOutlined } from '@mui/icons-material';
 
+import { useRecoilValue } from "recoil";
+
 import WordCountHeader from './WordCountHeader';
 import TextInput from './TextInput'
-import { WordCounterItemType } from '../state/counterState';
 
+
+import { useWordCounterList, wordCounterListState } from "../state/counterState";
+import { WordCounterItemType } from '../state/counterState';
 
 type Props = {
     counter: WordCounterItemType;
 }
 
 const WordCounter: React.FC<Props> = ({ counter }) => {
+  const wordCounters = useRecoilValue(wordCounterListState);
+  const { deleteCounter } = useWordCounterList();
+  const id = React.useMemo(
+    () => wordCounters.findIndex((listItem) => listItem === counter),
+    [counter, wordCounters]
+  );
+  const deleteItem = useCallback(() => {
+    deleteCounter(id);
+}, [deleteCounter, id]);
+  
     return (
         <Paper elevation={4}>
         <div>
           <WordCountHeader
             onActive={counter.isCounted}
+            deleteItem={deleteItem}
           />
         </div>
         <div>
