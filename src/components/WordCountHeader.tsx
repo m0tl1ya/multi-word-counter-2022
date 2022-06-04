be8989b8cfa8ddfa8bf086004a14abbbdfec1fec
tmoriya <moriya.takayasu514@gmail.com>
2022-06-04 16:30:04 +0900

    add deleteItem property

--- src/components/WordCountHeader.tsx
+++ src/components/WordCountHeader.tsx
@@ -1,30 +1,37 @@
 import * as React from 'react';
+
 import AppBar from '@mui/material/AppBar';
 import Typography from '@mui/material/Typography';
 
 import { IconButton, Switch } from '@mui/material';
 import { Clear } from '@mui/icons-material';
 
+import { useRecoilValue } from "recoil";
+
+import { useWordCounterList, wordCounterListState } from "../state/counterState";
+
+
 type Props = {
     onActive: boolean;
+    deleteItem: () => void,
 }
 
 const WordCountHeader: React.FC<Props> = (props) => {
     return (
-    <div>
-        {/* {element} */}
-        <Switch
-            checked={props.onActive}
-        // onChange={() => switchCount()}
-        />
-        <IconButton
-            aria-label="Delete"
-        // onClick={() => deleteCounter(props.id)}
-        >
-            <Clear />
-        </IconButton>
+        <div>
+            {/* {element} */}
+            <Switch
+                checked={props.onActive}
+            // onChange={() => switchCount()}
+            />
+            <IconButton
+                aria-label="Delete"
+            onClick={props.deleteItem}
+            >
+                <Clear />
+            </IconButton>
 
-    </div>
+        </div>
     );
 }
 
