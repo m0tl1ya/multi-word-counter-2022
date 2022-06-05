53f2c376d03241897bd9715a6087a4816694ba7e
tmoriya <moriya.takayasu514@gmail.com>
2022-06-06 00:22:27 +0900

    add count mode state

--- src/state/modeState.ts
+++ src/state/modeState.ts
@@ -1,5 +1,24 @@
 import { useCallback } from "react";
-import { atom, useSetRecoilState } from "recoil";
+import { atom, useRecoilState } from "recoil";
 
+export const modeValue = {
+    WORDS: "Words",
+    CHARACTER: "Characters",
+    CHARACTER_WITHOUT_SPACES: "Characters without spaces"
+};
 
-export {}
+export type CountModeType = typeof modeValue[keyof typeof modeValue];
+
+export const countModeState = atom<CountModeType>({
+    key: 'CountModeState',
+    default: modeValue.CHARACTER,
+})
+
+export const useCountMode = () => {
+    const [countMode, setCountMode] = useRecoilState(countModeState);
+    const switchCountMode = (value: CountModeType) => {
+        setCountMode(value);
+    }
+    return {countMode, switchCountMode};
+};
+
