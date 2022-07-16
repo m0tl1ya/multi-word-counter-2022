29db54cea3d5ec483dd792c5b6ad4fc4b0d1049d
m0tl1ya <moriya.takayasu514@gmail.com>
2022-07-16 17:07:01 +0900

    add selector test

--- src/state/counterStatsState.test.ts
+++ src/state/counterStatsState.test.ts
@@ -1,35 +1,57 @@
 import { renderHook } from '@testing-library/react-hooks';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, snapshot_UNSTABLE } from 'recoil';
 import { act, cleanup, fireEvent, render } from '@testing-library/react';
 import { RecoilRoot } from "recoil";
 
-import { useWordCounterList, WordCounterItemType } from "./counterState";
-import { CounterStatsType, totalStatsState } from "./counterStatsState"
+import { wordCounterListState, useWordCounterList, WordCounterItemType } from "./counterState";
+import { CounterStatsType, totalStatsState } from "./counterStatsState";
 
 afterEach(() => cleanup());
 
+
+
 describe('Test counter stats', () => {
-    const testStringFirst: string = "a b";
-    const testStringSecond: string = "c d e";
+    const expectedInitialStats: CounterStatsType = {
+        "totalNumOfCharacters": 0,
+        "totalNumOfCharactersWithoutSpace": 0,
+        "totalNumOfWords": 0
+    };
+    const testCase = [
+        {
+            id: 0,
+            text: "a b",
+            numWords: 0,
+            numCharacters: 0,
+            numCharactersWithoutSpaces: 0,
+            isCounted: true
+        },
+        {
+            id: 1,
+            text: "c d ef",
+            numWords: 0,
+            numCharacters: 0,
+            numCharactersWithoutSpaces: 0,
+            isCounted: true
+        }
+    ]
+    const expectedTestedStats: CounterStatsType = {
+        "totalNumOfCharacters": 9,
+        "totalNumOfCharactersWithoutSpace": 6,
+        "totalNumOfWords": 5
+    };
     test('initial num of counters.', () => {
-        const { result } = renderHook(() => useWordCounterList(), {
-            wrapper: RecoilRoot,
-        });
-        act(() => {
-            result.current.addCounterBelow();
-        })
-        const counterFirst: WordCounterItemType = result.current.wordCounters[0];
-        const counterSecond: WordCounterItemType = result.current.wordCounters[1];
-        act(() => {
-            result.current.addCounterBelow();
-            result.current.editText(0, counterFirst, testStringFirst);
-            result.current.editText(0, counterSecond, testStringSecond);
-        })
-        // const totalNumOfCharacters = renderHook(() => useRecoilValue(totalStatsState), {
-        //     wrapper: RecoilRoot,
-        // });
+        const initialSnapshot = snapshot_UNSTABLE();
+        expect(initialSnapshot.getLoadable(totalStatsState).valueOrThrow()).toStrictEqual(expectedInitialStats);
         
-        expect(8).toEqual(8);
+        const release = initialSnapshot.retain();
+        try {
+            const testSnapshot = snapshot_UNSTABLE(({ set }) => set(wordCounterListState, testCase));
+            expect(testSnapshot.getLoadable(totalStatsState).valueOrThrow()).toStrictEqual(expectedTestedStats);
+        } catch (error) {
+            
+        }finally {
+            release();
+        }
     });
 });
 
